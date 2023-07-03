4d8f1f35b96e3b021e37db3f8a2c7d33e3528bc5
curung <138360467+curung@users.noreply.github.com>
2023-07-03 18:34:31 +0900

    Update tut1.lua

--- lua/tut1.lua
+++ lua/tut1.lua
@@ -1,12 +1,4 @@
--- Cheat Engine Lua 스크립트
 
--- 타겟 프로세스와 Cheat Engine 연결
-local process = getProcessByName("target_process.exe") -- 대상 프로세스의 이름을 적어주세요
-if process == nil then
-    print("대상 프로세스를 찾을 수 없습니다.")
-    return
-end
-local openedProcess = openProcess(process.ID)
 
 -- 메모리에서 값을 찾고 변경
 local initialValue = 100 -- 변경하고자 하는 값의 초기값
@@ -15,11 +7,8 @@ local newValue = 5000 -- 변경할 값
 local address = getAddress(initialValue) -- 값을 찾을 메모리 주소 검색
 if address ~= nil then
     writeInteger(address, newValue) -- 메모리 값 변경
-    print("값을 변경했습니다.")
+    print("clear")
 else
-    print("값을 찾을 수 없습니다.")
+    print("falled!!")
 end
-
--- Cheat Engine와 연결 종료
-closeProcess(openedProcess)
 
